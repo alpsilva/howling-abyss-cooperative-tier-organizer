160e7e762d3db3f98d13292b436f43711fdd2f7e
André Luiz <alps2@cin.ufpe.br>
2023-06-12 14:42:23 -0300

    mocked db

---
+++ backend/aws-ha-tier-list-organizer/database/db_mock.py
@@ -0,0 +1,41 @@
+# This mock consists of:
+# a unique party id
+# Each party id has tierlists of different members of the party (each member has a unique name too)
+
+
+db = {
+    "party_1_id": {
+        "party_name": "Party 1",
+        "members": {
+            "member_1_id": {
+                "member_name": "raulsao",
+                "tier_list": {}
+            },
+            "member_2_id": {
+                "member_name": "Caio",
+                "tier_list": {}
+            },
+            "member_3_id": {
+                "member_name": "Wolf",
+                "tier_list": {}
+            }
+        }
+    },
+    "party_2_id": {
+        "party_name": "Party 2",
+        "members": {
+            "member_1_id": {
+                "member_name": "vini",
+                "tier_list": {}
+            },
+            "member_2_id": {
+                "member_name": "Clara",
+                "tier_list": {}
+            },
+            "member_3_id": {
+                "member_name": "Du",
+                "tier_list": {}
+            }
+        }
+    },
+}
