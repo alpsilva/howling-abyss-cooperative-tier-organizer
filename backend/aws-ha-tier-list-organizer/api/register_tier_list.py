7c1d959e9446b0b778332616d255016a60dceb21
André Luiz <alps2@cin.ufpe.br>
2023-06-15 15:01:47 -0300

    Register tierlist (db enabled)

--- backend/aws-ha-tier-list-organizer/api/register_tier_list.py
+++ backend/aws-ha-tier-list-organizer/api/register_tier_list.py
@@ -1,21 +1,23 @@
-import json
 from utils import translate_tier_to_number
-from database import db_mock
+from config import parties_table_name
 from http import HTTPStatus
+import boto3
+import json
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(parties_table_name)
 
 def lambda_handler(event, context):
     """
     Receives from the body:
-    party_id: str,
-    member_id: str
+    party_name: str,
+    member_name: str
     tierlist: dict
     """
 
-    db = db_mock.db
-    
     body = json.loads(event["body"])
-    party = body["party_id"]
-    member = body["member_id"]
+    party = body["party_name"]
+    member = body["member_name"]
     raw_tier_list = body["tierlist"]
 
     tier_list = {}
@@ -29,9 +31,44 @@ def lambda_handler(event, context):
         
         tier_list[character] = number
     
-    db[party]["members"][member]["tier_list"] = tier_list
+    keys = { "party_name": party }
+    response = table.get_item(Key=keys)
+
+    if 'Item' not in response:
+        return {
+            "statusCode": HTTPStatus.NOT_FOUND,
+            "body": "Party not found."
+        }
+
+    update_expression = """
+    SET
+    members.#member = :new_tier_list
+    """
+
+    expression_names = {
+        "#member": member
+    }
+
+    expression_values = {
+       ":new_tier_list": tier_list
+    }
+
+    response = table.update_item(
+        Key={"party_name": party},
+        UpdateExpression=update_expression,
+        ExpressionAttributeNames=expression_names,
+        ExpressionAttributeValues=expression_values,
+        ReturnValues='ALL_NEW'
+    )
+
+    updated = response.get("Attributes", {})
+    if len(updated) == 0:
+        return {
+            "statusCode": HTTPStatus.INTERNAL_SERVER_ERROR,
+            "body": "Tier-list was not registered."
+        }
 
     return {
         "statusCode": HTTPStatus.OK,
-        "body": json.dumps(db[party]["members"][member]["tier_list"])
+        "body": "Tier-list registered successfully."
     }
